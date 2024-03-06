63b5c900f68ee6e6dfd7254005fd1ad8c304670d
jamesdunseith <jdunseith@gimbalabs.io>
2024-03-06 08:19:57 -0500

    Add calendar

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,9 +1,20 @@
 export default async function CalendarPage() {
   return (
-    <main className="items-center justify-center font-extrabold text-4xl" style={{ minHeight: "calc(100vh - 5rem)" }}>
+    <main className="items-center justify-center" style={{ minHeight: "calc(100vh - 5rem)" }}>
       <div className="card bg-primary text-primary-content font-mono shadow-xl md:mx-5 md:mt-5 z-10 p-5">
-        <h1>Calendar</h1>
+        <h1 className="py-10 text-4xl">Andamio Public Calendar</h1>
+        <div className="">
+          <section className="realtive">
+            <iframe
+              src="https://teamup.com/ksso1wk6ysj9ireqau?title=Gimbalabs%20Calendar&showLogo=0&showSearch=0&showProfileAndInfo=0&showSidepanel=1&disableSidepanel=1&showTitle=0&showViewSelector=1&showMenu=0&showAgendaHeader=1&showAgendaDetails=0&showYearViewHeader=1"
+              width="100%"
+              height="800px"
+            ></iframe>
+          </section>
+        </div>
       </div>
     </main>
   );
 }
+
+// https://teamup.com/ksso1wk6ysj9ireqau
